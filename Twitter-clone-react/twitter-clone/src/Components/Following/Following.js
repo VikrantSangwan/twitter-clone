c182606879a88c4dcd30728d26d26be61b72c8a5
VikrantSangwan <v.sangwan9876@gmail.com>
2023-01-12 03:31:14 +0530

    started working on right-side

--- Twitter-clone-react/twitter-clone/src/Components/Following/Following.js
+++ Twitter-clone-react/twitter-clone/src/Components/Following/Following.js
@@ -1,7 +1,84 @@
 import React from "react";
+import "./Following.css";
 
 function Following() {
-  return <h1>This is Following section</h1>;
+  return (
+    <div className="rightcontainer">
+      <div className="searchbox">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="newhappening">
+        <h5>What's happening</h5>
+        <div className="feed">
+          <div className="feed-desc">
+            <p> Entertainment. Trending</p>
+            <p className="hashtag">#kabbadi</p>
+            <p>29.8k Tweets</p>
+          </div>
+          <div className="feed-logo">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-three-dots"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+            </svg>
+          </div>
+        </div>
+        <div className="feed">
+          <div className="feed-desc">
+            <p> Entertainment. Trending</p>
+            <p className="hashtag">#kabbadi</p>
+            <p>29.8k Tweets</p>
+          </div>
+          <div className="feed-logo">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-three-dots"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+            </svg>
+          </div>
+        </div>
+        <div className="feed">
+          <div className="feed-desc">
+            <p> Entertainment. Trending</p>
+            <p className="hashtag">#kabbadi</p>
+            <p>29.8k Tweets</p>
+          </div>
+          <div className="feed-logo">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-three-dots"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Following;
