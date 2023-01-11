774170bbf7b128de2e80daeecf41043ecf043863
VikrantSangwan <v.sangwan9876@gmail.com>
2023-01-11 20:44:12 +0530

    twitter-clone using reactjs

---
+++ Twitter-clone-react/twitter-clone/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import menu from "./Components/menu/menu";
+import follow from "./Components/follow/follow";
+import posts from "./Components/posts/posts";
+function App() {
+  return <h1>This is twitter-clone</h1>;
+}
+
+export default App;
