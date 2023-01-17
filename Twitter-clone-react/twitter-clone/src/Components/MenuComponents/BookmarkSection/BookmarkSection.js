deb7e24992978cc3c702fae8c44c40e9cb8cc5d6
VikrantSangwan <v.sangwan9876@gmail.com>
2023-01-17 11:13:30 +0530

    Bookmark Section Completed

--- Twitter-clone-react/twitter-clone/src/Components/MenuComponents/BookmarkSection/BookmarkSection.js
+++ Twitter-clone-react/twitter-clone/src/Components/MenuComponents/BookmarkSection/BookmarkSection.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./BookmarkSection.css";
 
 function BookmarkSection() {
-  return <h1>This is Bookmark Section.</h1>;
+  return (
+    <div className="bookmarkcontainer">
+      <div className="bookmarktopcont">
+        <div>
+          <p> Bookmarks</p>
+          <p>@Vikrant51113416</p>
+        </div>
+      </div>
+      <div className="bookmarkmaincont">
+        <div>
+          <div className="notweetimg">
+            <img
+              src="https://abs.twimg.com/responsive-web/client-web/book-in-bird-cage-400x200.v1.366bcfc9.png"
+              alt=""
+            />
+          </div>
+          <div className="notweetmsg">
+            <h2>Save Tweets for later</h2>
+            <p>
+              Don't let the good ones fly away! Bookmark Tweets to easily find
+              them again in the future.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookmarkSection;
